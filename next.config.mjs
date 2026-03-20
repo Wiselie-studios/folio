9e6b9b2c216a4eb631a2db47b8423a3a29e4549a
Michael Jones Jr. <michaeljonesjr.@Black-Panther.local>
2026-03-19 21:42:40 -0700

    Folio v1 launch

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,19 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 }
-
 export default nextConfig
+```
+
+Save it (`Cmd + S`), then back in the terminal run:
+```
+git add .
+git commit -m "Fix image config"
+git push
